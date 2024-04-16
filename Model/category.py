ea6d24f2e5aaf67f0a9c76168d118856843bfa37
Fomekong_Tido <tidojoyce7@gmail.com>
2024-04-16 13:18:43 +0200

    created the category table of the frontend model

---
+++ Model/category.py
@@ -0,0 +1,65 @@
+import os
+import sqlite3
+import json
+import requests
+
+from .constants import SERVER_URL
+
+class Category:
+    ENDPOINT = "/categories/"
+
+    def __init__(self, id=None, name=None) -> None:
+        self.id = id
+        self.name = name
+
+    def save(self):
+        url = f"{SERVER_URL}{self.ENDPOINT}"
+
+        payload = {'name': self.name}
+        headers = {}
+
+        if not self.id: # save to the backend with a POST request
+            response = requests.request("POST", url, headers=headers, data=payload)
+
+            data = json.loads(response.text)
+            self.id = data['id']
+        else: # update a particular subject
+            url += str(self.id)
+            response = requests.request("PATCH", url, headers=headers, data=payload)
+
+
+    def read(id=None):
+        url = f"{SERVER_URL}{__class__.ENDPOINT}"
+        url += id if id else ''
+
+        payload = {}
+        headers = {}
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+        response = json.loads(response.text)
+
+        if id:
+            expense = __class__(**response)
+            return expense
+        else:
+            expenses = []
+
+            for result in response:
+                expense = __class__(**result)
+                expenses.append(expense)
+            
+            return expenses
+
+    def delete(self):
+        url = f"{SERVER_URL}{self.ENDPOINT}{self.id}"
+
+        payload, headers = {}, {}
+
+        response = requests.request("DELETE", url, headers=headers, data=payload)
+
+        try:
+            response.raise_for_status()
+
+            self.id = None
+        except Exception as e:
+            raise e
